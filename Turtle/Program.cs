ab9ccd289eef832f4cb9818e94778fdb4d550879
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2022-12-22 19:11:08 +0200

    C# Trainning

---
+++ Turtle/Program.cs
@@ -0,0 +1,53 @@
+﻿// Will be discussed later in the course
+
+/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ */
+
+// See https://aka.ms/new-console-template for more information
+
+
+// 1. Draw a Shape
+Console.WriteLine("   /|");
+Console.WriteLine("  / |");
+Console.WriteLine(" /  |");
+Console.WriteLine("/___|");
+
+
+
+//2. ADD VARIABLES and types untill 29:45
+string name = "Andgoedu";
+int age = 24;
+
+Console.WriteLine("My name is " + name);
+Console.WriteLine("I am " + age + "years old" );
+Console.WriteLine("And i like variables that are named after me");
+Console.WriteLine("Which is " + name);
+
+// 
+
+
+namespace Turtle
+{   // class is just a  container for all the code to be executed
+    class Program
+    {
+        static void Main(string[] args)
+            // Main is a method or a function which is another container for code 
+        {
+
+        // C# Variables
+
+
+
+
+
+            // Makes Console stay open to ReadLine
+            Console.ReadLine();
+        }
+    }
+}
