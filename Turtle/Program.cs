b50d3c2976f0744abf042f3101ec8fbb2dc40bc6
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2022-12-28 02:09:11 +0200

    ADDED 15. switch statements

--- Turtle/Program.cs
+++ Turtle/Program.cs
@@ -79,24 +79,46 @@ Console.WriteLine(Math.Min(5, 20)); // Min Value
 
 
 //  6. GET USER INPUT 
+Console.WriteLine("Get User Inputs >>>>>>>>>");
 Console.Write("Kindly , Enter your name: ");
 string userName = Console.ReadLine(); // Data stored into the name variable
 Console.Write("Enter your age Please : ");
 string userAge = Console.ReadLine();
 Console.WriteLine("Hello " + userName + " your " + userAge + " years old !");
 
+//12. If Statements 
+bool isMale = true;
+bool isTall = true;
+
+if (isMale && isTall)
+{
+    Console.WriteLine("Gender is Male and Tall");
+}
+else if (isMale && !isTall)
+{
+    Console.WriteLine("Gender is Male But Not Tall");
+}
+else if (!isMale && isTall)
+{
+    Console.WriteLine("You are not a male but you are Tall");
+}
+else
+{
+    Console.WriteLine("Your either not a male nor tall");
+};
 
 //  7. Building a Calculator 
 
-int num = Convert.ToInt32("45"); // Converts string to integer // dont add any other charachters than numbers in between " " or it will not work will throw an error for format
-Console.WriteLine(num + 6);
-
+//int num = Convert.ToInt32("45"); // Converts string to integer // dont add any other charachters than numbers in between " " or it will not work will throw an error for format
+//Console.WriteLine(num + 6);
+Console.WriteLine("Calculator >>>>>>>>>");
 Console.Write(" Enter: A Number: ");
 double num1 = Convert.ToDouble(Console.ReadLine());
 Console.Write(" Enter A 2nd Number: ");
 double num2 = Convert.ToDouble(Console.ReadLine());
 
 Console.WriteLine(num1 * num2);
+
 
 
 //  8. Create A MadLib Game Untill 01:21:16 
@@ -116,28 +138,200 @@ Console.WriteLine("Violets are " + pluralNoun);
 Console.Write("I love " + celebrity);
 
 
-//  9. Arrays for large amounts of data
+//  9. Arrays for large amounts of data 01:26:18
 
 int[] luckyNumbers = {5, 7 , 7 ,12 , 16 , 28 };
+luckyNumbers[1] = 500;
+
+string[] friends = new string[10]; // friends array is going to hold 10 values
+friends[0] = " John";
+friends[1] = "Jane";
 
 Console.WriteLine(luckyNumbers[0]);
 
-Console.ReadLine();            // Makes Console stay open to ReadLine
 
+
+
+//  10. Methods -  A little block of code that performs a specific task
+
+
+
+
+
+
+
+
+static void Greet(string name, int age)
+{
+    Console.WriteLine("Hi There " + name + "You are " + age);
+}
+
+
+
+
+
+
+
+
+
+// 11. Return statements
+
+// GETS THE CUBED VALUE 2 ^ 3 = 2 * 2 * 2 
+static int cube (int num)
+
+{
+    int result = num * num * num;
+    return result;
+
+}
+
+
+
+//12. If Statements 
+
+if (isMale && isTall)
+{
+    Console.WriteLine("Gender is Male and Tall");
+}
+else if (isMale && !isTall)
+{
+    Console.WriteLine("Gender is Male But Not Tall");
+}
+else if (!isMale && isTall)
+{
+    Console.WriteLine("You are not a male but you are Tall");
+}
+else
+{
+    Console.WriteLine("Your either not a male nor tall");
+};
+
+
+
+//13. If statements Comparision 
+// Max method which one is bigger
+Console.WriteLine(">>>>>>GetMax Method");
+static int GetMax(int num1, int num2, int num3)
+{
+    int result;
+    if (num1 > num2) {
+        result = num1;
+    }
+    else if (num2 >= num1 && num2 >= num3)
+    {
+        result = num2;
+    }else
+    {
+        result = num3;
+    }
+   
+    return result;
+}
+
+
+Console.WriteLine(GetMax(2, 10, 4));
+
+//14 . Better Calculator
+Console.Write(" Better Calculator ");
+
+
+Console.Write("Enter a Number: ");
+double numb1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter Operator: ");
+string oper = Console.ReadLine();
+
+
+Console.Write("Enter a 2nd Number: ");
+double numb2 = Convert.ToDouble(Console.ReadLine());
+
+if (oper == "+")
+{
+    Console.WriteLine(numb1 + numb2);
+}else if (oper == "-")
+{
+    Console.WriteLine(numb1 - numb2);
+}else if (oper == "/")
+{
+    Console.WriteLine(numb1 / numb2);
+
+}
+else if (oper == "*") {
+    Console.WriteLine(numb1 * numb2);
+
+}
+else
+{
+    Console.WriteLine("Error invalid Operator");
+}
+
+
+
+//  15. Switch Statements 
+// Checking a bunch of different conditions easily
+// Create method that will check the day of the week 
+
+
+
+static string GetDay(int dayNumber)
+{
+    string dayName;
+
+    switch (dayNumber)
+    {
+        case 0:
+            dayName = "Sunday";
+            break;
+        case 1:
+            dayName = "Monday";
+            break;
+        case 2:
+            dayName = "Tuesday";
+            break;
+        case 3:
+            dayName = "Wednesday";
+            break;
+        case 4:
+            dayName = "Thursday";
+            break;
+        case 5:
+            dayName = "Friday";
+            break;
+        case 6:
+            dayName = "Saturday";
+            break;
+        default:
+
+            dayName = "Invalid Day Number";
+            break;
+    }
+
+    return dayName;
+}
+
+//DEBUG SWITCH STATEMENT
+Console.WriteLine(GetDay(0));
+
+Console.ReadLine();            // Makes Console stay open to ReadLine
 
 namespace Turtle
 {   // class is just a  container for all the code to be executed
     class Program
     {
+   
+
         static void Main(string[] args)
         // Main is a method or a function which is another container for code 
         {
 
 
-
+            Console.ReadLine();            // Makes Console stay open to ReadLine
 
 
         }
+     
 
     }
+
 }
+
